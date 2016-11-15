6e1b37024a2327ffea6774ff260ec9e45a846676
KUNEJ <kunemark884@edu.gedsb.local>
2016-11-15 09:53:15 -0500

    added practice folder

---
+++ Practice/name, age, colour.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("What is your name?");
+
+            Console.WriteLine("How old are you?");
+ 
+            Console.WriteLine("What is your favourite colour");
+            
+
+            Console.WriteLine("Hi Bob your 3 and like brown.");
+            Console.WriteLine("That was right, right?");
+            Console.WriteLine("Well I dont care, just deal with it");
+            Console.ReadLine();
+        }
+    }
+}
